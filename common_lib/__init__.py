66ea6e4d24a00940f8f8feac0606d9142f2a989c
jmgb <jesus82c@gmail.com>
2025-04-19 11:03:46 +0200

    fix

--- common_lib/__init__.py
+++ common_lib/__init__.py
@@ -5,9 +5,3 @@ from .ai_request      import *
 from .gpt_prompts     import *
 from .shared_functions import *
 
-__all__ = [
-    *ai_models.__all__,
-    *ai_request.__all__,
-    *gpt_prompts.__all__,
-    *shared_functions.__all__,
-]
